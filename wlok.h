d6020a02446492847db5f50fe1300f4f2aa9f108
Thomas Albrecht <thomas@hpke.de>
2019-07-23 17:58:48 +0200

    Die erste Kommunikation war erfolgreich

--- wlok.h
+++ wlok.h
@@ -19,6 +19,15 @@ public slots:
   void readData();
 
 private:
+  void getSerialNumber( char *, quint16 & ) const;
+  void getSystemState( char *, quint16 & ) const;
+  void logoff( char *, quint16 & );
+  void parseMsg( const char *, quint16, char *, quint16 & );
+  void processX( const char *, char *, quint16 &  );
+  void replyHeader( char *, quint16, quint16 ) const;
+  void setBroadcastFlags( const char *, char *, quint16 & );
+  void unknownCommand( char *, quint16 & ) const;
+
   QUdpSocket *_udpSocket;
 };
 
